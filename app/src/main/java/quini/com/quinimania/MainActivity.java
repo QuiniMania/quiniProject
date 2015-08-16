6e8e7694d097eec061a8e70c76ce2ef1feaa7ac2
ValeriaGodoy <valeriagodoy2@gmail.com>
2015-08-16 03:11:26 -0300

    Completé el layout de la MainActivity

--- app/src/main/java/quini/com/quinimania/MainActivity.java
+++ app/src/main/java/quini/com/quinimania/MainActivity.java
@@ -1,11 +1,16 @@
 package quini.com.quinimania;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.Date;
+
+public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
