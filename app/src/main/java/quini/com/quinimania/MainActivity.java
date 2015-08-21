92d1f0ed712c33423e59fc6a2d06f62614435c37
ValeriaGodoy <valeriagodoy2@gmail.com>
2015-08-21 09:16:42 -0300

    agregado de activitys

--- app/src/main/java/quini/com/quinimania/MainActivity.java
+++ app/src/main/java/quini/com/quinimania/MainActivity.java
@@ -1,39 +1,124 @@
 package quini.com.quinimania;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.sql.Date;
+import java.util.Calendar;
 
 public class MainActivity extends Activity {
 
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //mostrar fecha actual:
+        Calendar calendar = Calendar.getInstance();
+        int dia = calendar.get(Calendar.DAY_OF_MONTH);
+        int mes = calendar.get(Calendar.MONTH);
+        mes = mes + 1;
+        int anio = calendar.get(Calendar.YEAR);
+
+        TextView fecha = (TextView) findViewById(R.id.textViewFechaHoy);
+
+        fecha.setText(dia + "/" + mes + "/" + anio);
+
+
+        //animacion logo:
+        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.animacion);
+
+        ImageView bordeSuperior = (ImageView) findViewById(R.id.bordeSuperiorLogo);
+        bordeSuperior.startAnimation(animacion);
+
+        ImageView bordeInferior = (ImageView) findViewById(R.id.bordeInferiorLogo);
+        bordeInferior.startAnimation(animacion);
+
+        //animar bolas
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.animacion2);
+
+        ImageView bola_fuccia = (ImageView) findViewById(R.id.bola_fuccia);
+        bola_fuccia.startAnimation(animacion2);
+
+        Animation animacion3 = AnimationUtils.loadAnimation(this, R.anim.animacion3);
+        ImageView bola_amarilla = (ImageView) findViewById(R.id.bola_amarilla);
+        bola_amarilla.startAnimation(animacion3);
+
+        //pasar a JugarActivity
+
+        ImageButton botonJugar = (ImageButton) findViewById(R.id.imageButton);
+
+        botonJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, JugarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //pasar a MisJugadasActivity
+
+        ImageButton botonMisJugadas = (ImageButton) findViewById(R.id.imageButton3);
+
+        botonMisJugadas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MisJugadasActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //pasar a UltimoSorteoActivity
+
+        ImageButton botonUltimoSorteo = (ImageButton) findViewById(R.id.imageButton4);
+
+        botonUltimoSorteo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UltimoSorteoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //pasar a LosSalidoresActivity
+
+        ImageButton botonLosSalidores = (ImageButton) findViewById(R.id.imageButton2);
+
+        botonLosSalidores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LosSalidoresActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+
+        if (id == R.id.opcion_jugar)
+        {
             return true;
         }
 
